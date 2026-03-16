3dd07bf16b1c0ea588b5a2b45dfe20734d1f20d4
Guilherme <guilherme@spotify.com>
2026-03-16 17:35:15 +0100

    Update run script

--- sbin/local/est-o1.sh
+++ sbin/local/est-o1.sh
@@ -6,11 +6,25 @@ PARENT_DIR=$DIR/../..
 BASE=drmdp
 
 TIMESTAMP=`date +%s`
-python $PARENT_DIR/src/$BASE/dfdrl/est_o1.py \
+
+set -xe
+
+DIR=$(dirname $0)
+PARENT_DIR=$DIR/../..
+BASE=drmdp
+
+TIMESTAMP=`date +%s`
+ray job submit \
+    --address http://127.0.0.1:8265 \
+    --working-dir $PARENT_DIR \
+    --runtime-env-json='{"py_modules":["src/drmdp"], "excludes": [".git"]}' \
+    -- \
+    python $PARENT_DIR/src/$BASE/dfdrl/est_o1.py \
         --delay=3 \
         --num-steps=100 \
         --train-epochs=100 \
         --output-dir=$HOME/fs/$BASE/dfdrl/$TIMESTAMP \
         --task-prefix $TIMESTAMP \
+        --num-runs=3 \
         --log-episode-frequency=5 \
         --use-seed
