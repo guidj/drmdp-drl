e3f92f89015ace588d4581ebf28cfb71f5d7fd68
Guilherme <guilherme@spotify.com>
2026-04-22 09:54:26 +0200

    Script for o4

---
+++ sbin/local/rest-o4.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -xe
+
+DIR=$(dirname $0)
+PARENT_DIR=$DIR/../..
+BASE=drmdp
+
+TIMESTAMP=`date +%s`
+mkdir -p $HOME/fs/$BASE/dfdrl-rest/o4/$TIMESTAMP
+ray job submit \
+    --address http://127.0.0.1:8265 \
+    --working-dir $PARENT_DIR \
+    --runtime-env-json='{"py_modules":["src/drmdp"], "excludes": [".git"]}' \
+    -- \
+    python $PARENT_DIR/src/$BASE/dfdrl/est_o4.py \
+        --delay=3 \
+        --buffer-num-steps=10000 \
+        --train-epochs=1000 \
+        --output-dir=$HOME/fs/$BASE/dfdrl-rest/o4/$TIMESTAMP \
+        --num-runs=3 \
+        --log-episode-frequency=5
